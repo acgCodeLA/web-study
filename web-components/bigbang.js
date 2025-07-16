f986ff433c5306ea6ae337e6a5f01b1f9d5531ba
mingchen <chenming@lenovo-ar.com>
2025-07-16 23:15:25 +0800

    host、host-context、slotted、part伪类的学习

--- web-components/bigbang.js
+++ web-components/bigbang.js
@@ -1,8 +1,34 @@
 const template = document.createElement('template')
 template.innerHTML = `
+  <style>
+    /* @import url(); */
+    h1 {
+      background-color: yellow;
+    }
+    :host {
+      /* for the shadowRoot */
+      display: block;
+      background-color: lightblue;
+    }
+    :host(big-bang) {
+      background-color: lightgreen;
+    }
+    :host-context(main){
+      background-color: lightcoral;
+    }
+    ::slotted(*) {
+      color: red;
+    }
+    :host-context(main) ::slotted(h2) {
+      color: blue !important;
+    }
+    ::slotted(ul) {
+      color: green;
+    }
+  </style>
   <div>
     <slot name="list">Default text if not list slot used in HTML</slot>
-    <h1>Big Bang Theory</h1>
+    <h1 part="topper">Big Bang Theory</h1>
     <slot name="title">Default text if not title slot used in HTML</slot>
   </div>
 `
