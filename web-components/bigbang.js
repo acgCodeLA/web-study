5f956c76310622a5a84e0fa2d8dd80280ecbfdc8
mingchen <chenming@lenovo-ar.com>
2025-07-23 15:29:50 +0800

    实现组建的属性

--- web-components/bigbang.js
+++ web-components/bigbang.js
@@ -26,7 +26,7 @@ template.innerHTML = `
       color: green;
     }
   </style>
-  <div>
+  <div class="root">
     <slot name="list">Default text if not list slot used in HTML</slot>
     <h1 part="topper">Big Bang Theory</h1>
     <slot name="title">Default text if not title slot used in HTML</slot>
@@ -34,11 +34,41 @@ template.innerHTML = `
 `
 
 class BigBang extends HTMLElement {
+  root;
   constructor() {
     super()
     const shadowRoot = this.attachShadow({ mode: 'closed' })
     let clone = template.content.cloneNode(true);
     shadowRoot.append(clone);
+    this.root = shadowRoot.querySelector('.root');
+  }
+
+  static get observedAttributes() {
+    return ['color', 'test-text']
+  }
+
+  get color() {
+    return this.getAttribute('color')
+  }
+  set color(value) {
+    this.setAttribute('color', value)
+  }
+
+  get testText() {
+    return this.getAttribute('test-text')
+  }
+  set testText(value) {
+    this.setAttribute('test-text', value)
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    if (name === 'color') {
+      this.root.setAttribute('style', `${`${oldValue};` || ''}background-color: ${newValue}`);
+    }
+    if (name === 'test-text') {
+      this.root.querySelector('h1').textContent = newValue;
+    }
   }
 }
 
--- web-components/main.js
+++ web-components/main.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('main.js loaded');
+  const bb = document.querySelectorAll('big-bang');
+  if (bb.length > 0) {
+    bb.forEach(el => {
+      el.addEventListener('click', clickBB);
+    })
+  }
 })
+
+function clickBB(ev) {
+  const bb = ev.target;
+  bb.color = 'black';
+  bb.testText = 'Clicked!';
+}
